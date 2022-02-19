88ea8ccd9224cbdd5cd5f0dde4ddabb6698bc9f6
Namee2810 <dvn281002@gmail.com>
2022-02-19 11:42:05 +0700

    update sw

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -73,7 +73,9 @@ registerRoute(
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+  console.log("message event:", event)
+
+  if (event.data === "SKIP_WAITING") {
     self.skipWaiting()
   }
 })
