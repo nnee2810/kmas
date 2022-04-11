498d97e4aeceaa1bb97e42511a4c091be28b2914
Namee2810 <dvn281002@gmail.com>
2022-04-11 18:32:43 +0700

    update service

--- src/layouts/Home/components/Header/MenuUser.tsx
+++ src/layouts/Home/components/Header/MenuUser.tsx
@@ -9,7 +9,6 @@ import {
 } from "@chakra-ui/react"
 import { useAppSelector } from "hooks/useAppStore"
 import React from "react"
-import { AiOutlineReload } from "react-icons/ai"
 import { IoLogOutOutline } from "react-icons/io5"
 import { userSelector } from "store/reducers/user"
 import ModalLogout from "./ModalLogout"
@@ -18,7 +17,7 @@ export default function MenuUser() {
   const {
     profile: { fullName },
   } = useAppSelector(userSelector)
-  const [isOpen, setIsOpen] = useBoolean()
+  const [openModalLogout, setOpenModalLogout] = useBoolean()
 
   return (
     <>
@@ -36,19 +35,16 @@ export default function MenuUser() {
           />
         </MenuButton>
         <MenuList>
-          <MenuItem icon={<AiOutlineReload fontSize="18" />}>
-            Cập nhật lịch học
-          </MenuItem>
           <MenuItem
             icon={<IoLogOutOutline fontSize="20" />}
             _hover={{ bg: "red", color: "white" }}
-            onClick={setIsOpen.on}
+            onClick={setOpenModalLogout.on}
           >
             Đăng xuất
           </MenuItem>
         </MenuList>
       </Menu>
-      <ModalLogout isOpen={isOpen} onClose={setIsOpen.off} />
+      <ModalLogout isOpen={openModalLogout} onClose={setOpenModalLogout.off} />
     </>
   )
 }
--- src/modules/login/services/getLessons.ts
+++ src/modules/login/services/getLessons.ts
@@ -1,7 +1,7 @@
-import API from "configs/network"
+import API from "configs/service"
 import { Student } from "types/Student"
 import { GetLessonsDto } from "../dto/get-lessons.dto"
 
 export async function getLessons(params: GetLessonsDto): Promise<Student> {
-  return await API.post("/lessons", params)
+  return (await API.post("/lessons", params)).data
 }
