4c8c89784795b2246ab9505c75e84b0484d3831c
Nnee <dvn281002@gmail.com>
2023-08-21 17:08:34 +0700

    feature: add vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
 })
